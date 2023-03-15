78974642f269272dea171ec1df0e3fc5d49e9dfe
Kevin Bravo <kbravo1024@gmail.com>
2023-03-15 01:38:44 -0700

    complete arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 
 // Write your code here
+
+//multiply equation ewualing 62
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2; 
+
+//random creats a random int greater than 0
+let random = Math.ceil(Math.random() * 10);
+
+//mod equation calculates remainder that is equal to 4
+
+let num3 = 9;
+let num4 = 5;
+let mod = num3 % num4;
+
+// max will return 20 as the highest number in the set
+
+let max = Math.max(1, 20, 8, 9, 10)
